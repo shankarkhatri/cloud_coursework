a5b56bb732d0056a46da13dc837f9d5ceb9813bc
Shankar Khatri <=>
2017-02-28 12:24:12 +0000

    testing API

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask('testing application') 
-from app import views
+
+mytasks = [
+{
+'id': 1,
+'task': u'Finish Hello World',
+'session': u'Week 7','done': True
+}
+,
+{'id': 2,'task': u'Finish first API',
+'session': u'Week 7','done': False}]
+@app.route('/todo/api/tasks', methods=['GET'])
+
+def get_tasks():
+	return jsonify({'tasks': mytasks})
+#from app import views
