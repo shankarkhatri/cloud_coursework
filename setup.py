05304763d8e112a395a4a6e2b7ee6e633d717a93
Shankar Khatri <=>
2017-03-14 13:46:16 +0000

    import errors

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Shankar Khatri',
       author_email='test_email@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2','flask-wtf','unirest','bokeh','numpy'],
+      install_requires=['Flask>=0.7.2','flask-wtf','unirest','bokeh','numpy','six'],
       )
