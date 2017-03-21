26b7f43e3d53e8563239fc3289db54efe61fc5c5
Shankar Khatri <=>
2017-03-21 13:16:31 +0000

    updated mongodb

--- wsgi/app/database.py
+++ wsgi/app/database.py
@@ -3,13 +3,29 @@ from flask_pymongo import PyMongo
 from flask import jsonify
 import os
 
-#app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
-#app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
+app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
+app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
 
-app.config['MONGO_DBNAME'] = "sk323apitest1"
-app.config['MONGO_URI'] = "mongodb://admin:7jyadjEqTRHl@127.0.0.1:27017/sk323apitest1"
 mongo = PyMongo(app)
 
 @app.route('/database/collections', methods=['GET'])
 def get_all_databases():
 	return jsonify({'result' : mongo.db.collection_names()})
+
+
+@app.route('/database/personnel', methods=['GET'])
+def getallpersonnel():
+	collection = mongo.db.test
+	output = []
+	for doc in collection.find():
+		output.append({'Who' : doc['Name'], 'Job Role' : doc['Profession']})
+
+	return jsonify({'result' : output})
+
+
+@app.route('/database/add', methods=['GET'])
+def insert_into_db():
+        collection = mongo.db.test
+	collection.insert_one({'Name': 'TestingName', 'Profession': 'student'})
+
+
